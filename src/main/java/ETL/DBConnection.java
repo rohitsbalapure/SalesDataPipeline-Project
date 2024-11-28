0d3b6de932964d3668bd070195179d9ce0a9e2e8
Rohit Balapure <rohitbalapure@gmail.com>
2024-11-28 16:08:04 +0530

    Update DBConnection.java

--- src/main/java/ETL/DBConnection.java
+++ src/main/java/ETL/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://salesdata.c702mkqeyu9f.ap-south-1.rds.amazonaws.com:3306/salesdata";
-    private static final String USERNAME="admin";
-    private static final String PASSWORD = "Rohit2024";
+    private static final String USERNAME="*******";
+    private static final String PASSWORD = "******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
